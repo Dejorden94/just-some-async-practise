a9998beb6de954db80772534854d98a8c31bcff8
Dejorden <d.moerman@hotmail.nl>
2025-09-19 11:31:59 +0200

    Add basic functionality to fetch and display book data
    - Implement `bookData` function to fetch book data from Google APIs.
    - Update HTML structure to include a textarea for displaying data.
    - Add basic styling for `weather-data` article.
    - Configure TypeScript project settings.
    - Add `package-lock.json` and update dependencies in `package.json`.

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+const dataElement = document.getElementById("--weather-data");
+async function bookData() {
+    const bookDataUrl = `https://www.googleapis.com/auth/books`;
+    try {
+        const response = await fetch(bookDataUrl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+const dataToUse = bookData();
+if (dataElement !== null) {
+    dataElement.innerText = JSON.stringify(dataToUse);
+}
+console.log(dataToUse);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+const dataElement = document.getElementById("--weather-data");
+
 async function bookData() {
     const bookDataUrl = `https://www.googleapis.com/auth/books`
     try {
@@ -5,7 +7,16 @@ async function bookData() {
         if (!response.ok) {
             throw new Error('Network response was not ok')
         }
+        return await response.json();
     } catch (error) {
         console.log(error)
     }
 }
+
+
+
+const dataToUse = bookData();
+if(dataElement !== null) {
+    dataElement.innerText = JSON.stringify(dataToUse);
+}
+console.log(dataToUse);
