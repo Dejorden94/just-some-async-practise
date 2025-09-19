d59567fe9dc56ffaded5c2352dc98e2e39655019
Dejorden <d.moerman@hotmail.nl>
2025-09-19 11:43:58 +0200

    Update title, improve API fetch logic, and adjust `.gitignore`

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import {key} from "./secret"
 const dataElement = document.getElementById("--weather-data");
 
 async function bookData() {
-    const bookDataUrl = `https://www.googleapis.com/auth/books`
+    const bookDataUrl = `https://www.googleapis.com/auth/books`;
     try {
-        const response = await fetch(bookDataUrl)
+        const response = await fetch(bookDataUrl,
+            {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
         if (!response.ok) {
             throw new Error('Network response was not ok')
         }
